7f45ef7f3d1d8779303b2279ad1e074c5145d905
jinhongzhu <zjhong6666@gmail.com>
2025-01-12 12:02:05 +0800

    add removefromback

--- GT-DSI/assignment1.java
+++ GT-DSI/assignment1.java
@@ -144,6 +144,17 @@ public class ArrayList<T> {
      */
     public T removeFromBack() {
         // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+        try{
+            if (size == 0){
+                throw new NoSuchElementException("List is empty");
+            }
+            T temp = backingArray[size-1];
+            backingArray[size-1] = null;
+            size--;
+            return temp;
+        }catch (NoSuchElementException e){
+            throw new NoSuchElementException("List is empty");
+        }
     }
 
     /**
